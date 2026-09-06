849c505402adabf79c104ed7ed1d9f23e0c1dca2
theblueman003 <theblueman003@gmail.com>
2026-09-06 13:10:53 +0200

    add dummy main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+#include "esp_bt.h"
+#include "wiring.h"
+#include "config.h"
+#include "encoders.h"
+#include "lifecycle.h"
+#include "i2c.h"
+#include "ToF.h"
+
+void setup() {
+    // Your code here ...
+}
+
+void loop() {
+    // Your code here ...
+}
